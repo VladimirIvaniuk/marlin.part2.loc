a8f4451111806501af6d8c96fc80d8fbef007a4c
VladimirIvaniuk <ivanyuk85vladimir@gmail.com>
2021-04-18 19:37:53 +0300

    update task 2/ part 2

--- functions.php
+++ functions.php
@@ -14,56 +14,67 @@ function get_user_by_email($email)
     $result = $statement->fetch(PDO::FETCH_ASSOC);
     return $result;
 }
+
 /**
  * @param string $email
  * @description: добавить пользователя в базу
  * @param string $password
  * @return int
  */
-function add_users($email, $password){
+function add_users($email, $password)
+{
     $pdo = new PDO('mysql:host=localhost; dbname=marlin_part_2;', "root", "root");
     $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
     $statement = $pdo->prepare($sql);
-    $result = $statement->execute(["email"=>$email, "password"=>password_hash($password, PASSWORD_DEFAULT)]);
+    $result = $statement->execute(["email" => $email, "password" => password_hash($password, PASSWORD_DEFAULT)]);
     return $result;
 }
+
 /**
  * @param string $name (key)
  * @param string $message (значние, текст сообщения)
  * @description: подготовить флеш сообщение
  * @return value: null
  */
-function set_flash_massage($name, $message){
-    $_SESSION[$name]=$message;
+function set_flash_massage($name, $message)
+{
+    $_SESSION[$name] = $message;
     return null;
 }
+
 /**
  * @param string $name (ключ)
  * @description: вывести флеш сообщение
-// * @return value: null
+ * // * @return value: null
  */
-function display_flash_message($name){
-    if(isset($_SESSION[$name])){
+function display_flash_message($name)
+{
+    if (isset($_SESSION[$name])) {
         echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\">{$_SESSION[$name]}</div>";
     }
     unset($_SESSION[$name]);
 }
+
 /**
  * @param string $path (путь куда переадресовывать)
  */
-function redirect_to($path){
-    header('Location: '. $path);
+function redirect_to($path)
+{
+    header('Location: ' . $path);
     exit;
 }
-function register(){
-    $user= get_user_by_email($_POST['email']);
-    if(!$user){
-        if(add_users($_POST['email'], $_POST['password'])){
+
+function register($data)
+{
+    $email = $data['email'];
+    $password = $data['password'];
+    $user = get_user_by_email($email);
+    if (!$user) {
+        if (add_users($email, $password)) {
             set_flash_massage("success", "Успешная регистрация");
             redirect_to("page_login.php");
         }
-    }
-    else{
+    } else {
         set_flash_massage('danger', "Этот эл. адрес уже занят другим пользователем.");
         redirect_to("/page_register.php");
     }
